ead8e459a37c23a71f83830a662af4ef77ebabdd
mikykonst <mikykonst66@gmail.com>
2017-11-11 00:28:38 +0200

    Some functional was added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Burger', 'The best burger ever!', 'https://cdn.pixabay.com/photo/2017/08/08/18/09/burger-2612137_960_720.jpg')
+  ];
 
   constructor() { }
 
